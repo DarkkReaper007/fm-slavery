a6f98501f3b9ee5a71c3338971b0b4d8d7cce0ae
DarkkReaper007 <139695435+DarkkReaper007@users.noreply.github.com>
2024-07-31 21:56:26 +0530

    Update slavery.py

--- slavery.py
+++ slavery.py
@@ -1,9 +1,25 @@
-#install dependencies
+#Install dependencies
 #pip install ultralytics
+
 from ultralytics import YOLO
-model_path = "" # path to the weights file (.pt file)
+
+
+model_path = ""  #path to the weights file
 model = YOLO(model_path)
-results = model.predict(source='', imgsz=640, conf=0.25) # put the folder that contains all the images
-for result in results:
-    print("Labels:", result.names)
-    print("Confidences:", result.boxes.conf)
+
+
+image_path = "" #path to the image
+while True:
+    results = model.predict(source=image_path, imgsz=640, conf=0.25)
+    for result in results:
+        if result.boxes:  # Check if there are any predictions
+            print(result.names)
+            print("Confidences:", result.boxes.conf.tolist())  
+        
+
+
+
+
+
+
+
